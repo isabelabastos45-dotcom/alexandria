95f832a722338e9b205ca2eceb6725b47ef194df
isabelabastos45-dotcom <isabela.bastos45@aluno.ifce.edu.br>
2026-06-23 11:12:34 -0300

    detalhes finais

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('profile'); 
+  if (!req.session.usuario) {
+    return res.redirect('/login');
+  }
+
+  res.render('profile', {
+    usuario: req.session.usuario
+  });
 });
 
 module.exports = router; 
